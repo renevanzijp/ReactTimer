a8ca84c73a9fea557824eff919d5934b790c8e18
vanzijp <vanzijp@gmail.com>
2017-01-01 17:12:39 +0100

    navbar styled

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,41 @@
+/*
+Creating and using new components:
+1) Create:
+    a) Create component like and expose it.
+    b) Add alias to webpack.config.js
+2) Use:
+    a require: var Navigation = require ("Navigation")
+    b) <Navigation/>
+ */
+
+var React = require("react");
+var {Link, IndexLink} = require("react-router");
+
+var Navigation =  () => {
+    return (
+        <div className="top-bar">
+            <div className="top-bar-left">
+                <ul className="menu">
+                    <li className="menu-text">
+                        React timer app
+                    </li>
+                    <li>
+                        <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+                    </li>
+                    <li>
+                        <Link to="/" activeClassName="active-link">Countdown</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+                <ul className="menu">
+                    <li className="menu-text">
+                        Created by <a href="https://github.com/renevanzijp" target="_blank">René van Zijp</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+module.exports = Navigation;
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-var webpack = require("webpack");
+var webpack = require('webpack');
 
 module.exports = {
     entry: [
-        "script!jquery/dist/jquery.min.js",
-        "script!foundation-sites/dist/foundation.min.js",
-        "./app/app.jsx"
+        'script!jquery/dist/jquery.min.js',
+        'script!foundation-sites/dist/foundation.min.js',
+        './app/app.jsx'
     ],
     externals: {
-      jquery: 'jQuery'
+        jquery: 'jQuery'
     },
     plugins: [
         new webpack.ProvidePlugin({
@@ -17,27 +17,33 @@ module.exports = {
     ],
     output: {
         path: __dirname,
-        filename: "./public/bundle.js"
+        filename: './public/bundle.js'
     },
     resolve: {
         root: __dirname,
         alias: {
-            Main: "app/components/Main.jsx",
-            applicationStyles: "app/styles/app.css"
+            Main: 'app/components/Main.jsx',
+            applicationStyles: 'app/styles/app.scss',
+            Navigation: 'app/components/Navigation.jsx',
+            Timer: 'app/components/Timer.jsx',
+            Countdown: 'app/components/Countdown.jsx',
+            Clock: 'app/components/Clock.jsx',
+            CountdownForm: 'app/components/CountdownForm.jsx',
+            Controls: 'app/components/Controls.jsx'
         },
-        extensions: ["", ".js", ".jsx"]
+        extensions: ['', '.js', '.jsx']
     },
     module: {
         loaders: [
             {
-                loader: "babel-loader",
+                loader: 'babel-loader',
                 query: {
-                    presets: ["react", "es2015", "stage-0"]
+                    presets: ['react', 'es2015', 'stage-0']
                 },
                 test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/
             }
         ]
     },
-    devtools: 'eval-source-map'
+    devtool: 'cheap-module-eval-source-map'
 };
