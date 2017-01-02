f9db9b9d8627b6c73a848888af6c6c755d9bfb1e
vanzijp <vanzijp@gmail.com>
2017-01-02 09:18:14 +0100

    testing howto described

--- app/tests/app.test.jsx
+++ app/tests/app.test.jsx
@@ -1,3 +1,14 @@
+/*
+ Testing:
+ 1) Install packages:
+        npm install karma@0.13.22 karma-chrome-launcher@0.2.2 karma-mocha@0.2.2 karma-mocha-reporter@2.0.0 karma-sourcemap-loader@0.3.7 karma-webpack@1.7.0 mocha@2.4.5 expect@1.14.0 --save-dev
+ 2) Write karma.conf.js
+ 3) Edit entry in package.json
+        "scripts": {
+        "test": "karma start",
+  4) Add tests in folder tests
+ */
+
 var expect = require('expect');
 
 
