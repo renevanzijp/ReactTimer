34bd6f751a3077f244c3857bd6ea8e287c496624
vanzijp <vanzijp@gmail.com>
2017-01-07 12:12:57 +0100

    CountdownForm added

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,11 +1,29 @@
 var React  = require("react");
 var Clock = require("Clock");
+var CountdownForm = require("CountdownForm");
 
 var Countdown = React.createClass({
+    getInitialState: function () {
+        return {
+            count: 0,
+            countdownStatus: 'stopped'
+        };
+    },
+    handleSetCountdown: function (seconds) {
+        this.setState({
+            count: seconds,
+            countdownStatus: 'started'
+        });
+    },
+
     render: function () {
+        var {count, countdownStatus} = this.state;
+        console.log("Status: ", countdownStatus);
         return (
         <div>
-            <Clock totalSeconds={127}/>
+            <Clock totalSeconds={count}/>
+            <CountdownForm onSetCountdown={this.handleSetCountdown} />
+
         </div>
         );
     }
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,14 +4,13 @@ var Navigation = require ("Navigation");
 var Main = (props) => {
     return (
         <div>
-            <div>
-                <div>
-                    <Navigation/>
+            <Navigation/>
+            <div className="row">
+                <div className="column small-centered medium-6 large-4">
                     {props.children}
                 </div>
             </div>
         </div>
     );
 }
-
 module.exports = Main;
